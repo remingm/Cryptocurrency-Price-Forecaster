1b722c48a5b88d5d4039ed755c1afeb04e883dc4
Marshall Powell <powellmarshall3@gmail.com>
2021-11-29 06:23:54 +0000

    finished front end design mvp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,22 @@
-import './App.css'
-import Navigation from './components/Navigation'
-import { Route, BrowserRouter as Router } from "react-router-dom"
-import Home from './Pages/Home'
-import About from './Pages/About'
-import CoinDetail from './Pages/Coin/CoinDetail'
+import "./App.css";
+import Navigation from "./components/Navigation";
+import { Route, BrowserRouter as Router, Redirect } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import CoinDetail from "./Pages/Coin/CoinDetail";
 
 function App() {
-
   return (
     <div>
       <Navigation></Navigation>
       <Router>
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact>
+          <Redirect to="/coins/btc" />
+        </Route>
         <Route path="/about" component={About} />
         <Route path="/coins/:coinId" component={CoinDetail} />
-
       </Router>
     </div>
-  )
+  );
 }
-export default App
+export default App;
--- frontend/src/Pages/Coin/CoinDetail.tsx
+++ frontend/src/Pages/Coin/CoinDetail.tsx
@@ -1,13 +1,81 @@
-import { useParams } from "react-router"
-import { Coin } from "./model"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { Coin, Bell, Prices } from "./model";
+import api from "./api";
+import Graph from "./Graph";
 
-const CoinDetail = (props: number) => {
-    const { coinId } = useParams<{ coinId: string | undefined }>()
-    console.log(coinId)
-    console.log('hi')
+const CoinDetail = (props: string) => {
+  const { coinId } = useParams<{ coinId: string }>();
+  const [error, setError] = useState("");
+  const [isLoaded, setLoaded] = useState(false);
+  const [coin, setCoin] = useState<Coin>();
+  const [needConvertedTime, setNeedConvertedTime] = useState(true);
+  const [pastPrices, setPastPrices] = useState<Prices>({});
+  const [predictionPrices, setPredictionPrices] = useState<Prices>();
+  useEffect(() => {
+    api.getCoin(coinId).then(
+      (resp) => {
+        setCoin(resp);
+        setLoaded(true);
+      },
+      (error) => {
+        setError(error);
+        setLoaded(true);
+      }
+    );
+  }, []);
+  useEffect(() => {
+    if (needConvertedTime) {
+      if (coin != null) {
+        setPastPrices(coin.pastPrices[Bell.CLOSE]);
+        setPredictionPrices(coin.predictionPrices[Bell.CLOSE]);
+      }
+    }
+  });
+  useEffect(() => {
+    if (needConvertedTime) {
+      if (pastPrices != null) {
+        for (const [key] of Object.entries(pastPrices)) {
+          const date = new Date(parseInt(key) * 1000);
+          const month = date.getMonth() + 1;
+          const day = date.getDate();
+          pastPrices[month + "/" + day] = pastPrices[key];
+          delete pastPrices[key];
+        }
+      }
+      if (predictionPrices != null) {
+        for (const [key] of Object.entries(predictionPrices)) {
+          const date = new Date(parseInt(key) * 1000);
+          const month = date.getMonth() + 1;
+          const day = date.getDate();
+          predictionPrices[month + "/" + day] = predictionPrices[key];
+          delete predictionPrices[key];
+        }
+        setNeedConvertedTime(false);
+      }
+    }
+  });
 
+  return (
+    <div className="relative pt-20 h-screen sm:px-10 max-w-6xl flex-col flex justify-between mx-auto">
+      {!isLoaded && <div>Loading...</div>}
+      {error && <div>{error}</div>}
 
-    return (<div>coin {coinId} fdsafd</div>)
-}
+      <div className="flex-1">
+        <p className=" text-3xl text-center">{coin?.name}</p>
+      </div>
 
-export default CoinDetail
+      <div className=" flex-grow">
+        {needConvertedTime && <div>Loading...</div>}
+        {!needConvertedTime && (
+          <Graph
+            pastPrices={pastPrices}
+            predictionPrices={coin?.predictionPrices[Bell.CLOSE]}
+          ></Graph>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CoinDetail;
