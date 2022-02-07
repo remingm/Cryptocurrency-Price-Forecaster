e707ca9c114ed6cef793326f2af51bdbcaf012da
remingm <michael.remington@outlook.com>
2022-02-06 16:19:05 -0800

    simple dashboard to view predictions stored in mongo

---
+++ ml_backend/dashboard/run.sh
@@ -0,0 +1,4 @@
+export MONGODB_URI='mongodb+srv://cluster0.p8o7k.mongodb.net/test?retryWrites=true&w=majority' &&
+export DB_PASSWORD= &&
+export DB_USERNAME=stonk-pix-db-user &&
+streamlit run mongo_dashboard.py
